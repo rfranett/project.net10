b38cbdc7fbf8cd7bf3dcc70500b3f26663b0ed28
rfranett <rfranett@gmail.com>
2023-12-01 14:10:20 -0300

    exeplos

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
 ﻿using project.net10.models;
 
-using System;
 
-class Program
-{
-    static void Main()
-    {
+ {
         Console.WriteLine("Digite um caractere:");
         string entrada = Console.ReadLine();
 
@@ -13,36 +9,78 @@ class Program
         {
             char caractere = entrada[0];
 
-            if (char.IsLetter(caractere))
+            switch (caractere)
             {
-                if (caractere == 'a' || caractere == 'e' || caractere == 'i' || caractere == 'o' || caractere == 'u' ||
-                    caractere == 'A' || caractere == 'E' || caractere == 'I' || caractere == 'O' || caractere == 'U')
-                {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
                     Console.WriteLine("É uma vogal");
-                }
-                else
-                {
-                    Console.WriteLine("É uma consoante");
-                }
-            }
-            else if (char.IsDigit(caractere))
-            {
-                Console.WriteLine("É um número");
-            }
-            else
-            {
-                Console.WriteLine("É um caractere especial");
+                    break;
+
+                default:
+                    if (char.IsDigit(caractere))
+                    {
+                        Console.WriteLine("É um número");
+                    }
+                    else
+                    {
+                        Console.WriteLine("É uma consoante ou caractere especial");
+                    }
+                    break;
             }
         }
         else
         {
             Console.WriteLine("Por favor, digite apenas um caractere.");
+        }}
+/*
+===================================================================================ex2
+{
+    Console.WriteLine("Digite um caractere:");
+    string entrada = Console.ReadLine();
+
+    if (entrada.Length == 1)
+    {
+        char caractere = entrada[0];
+
+        switch (caractere)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                Console.WriteLine("É uma vogal");
+                break;
+
+            case >= '0' and <= '9':
+                Console.WriteLine("É um número");
+                break;
+
+            default:
+                Console.WriteLine("É um caractere especial");
+                break;
         }
+    }
+    else
+    {
+        Console.WriteLine("Por favor, digite apenas um caractere.");
     }
 }
 
-
-/*
+=========================================================================ex1
 Console.WriteLine("Digite uma Letra");
 String Letra = Console.ReadLine();
 
--- obj/Debug/net8.0/project.net10.AssemblyInfo.cs
+++ obj/Debug/net8.0/project.net10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project.net10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+220dc204d095ce08290a486782735715358fe716")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdf4ef1b6a1b1e310818dadede4f2cc0f1d8b9f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("project.net10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project.net10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
