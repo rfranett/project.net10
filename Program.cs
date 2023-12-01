220dc204d095ce08290a486782735715358fe716
rfranett <rfranett@gmail.com>
2023-12-01 11:52:13 -0300

    switch1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using project.net10.models;
+
+
+Console.WriteLine("Digite uma Letra");
+String Letra = Console.ReadLine();
+
+switch (Letra)
+{   case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+    case "A":
+    case "E":
+    case "I":
+    case "O":
+    case "U":
+        Console.WriteLine("Vogal");
+        break;
+        
+
+    default:
+        Console.WriteLine("Não é uma Vogal");
+        break;
+
+
+    }
